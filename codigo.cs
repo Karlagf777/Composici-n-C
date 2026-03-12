14f42dc2dac1a732949837cf0732b9fc482e7a76
KarlaMinely7 <karla.gf.upaep@gmail.com>
2026-03-12 08:37:58 -0600

    Create codigo.cs

---
+++ codigo.cs
@@ -0,0 +1,31 @@
+using System
+
+public class Computadora
+{
+    //composición
+    private Procesador procesador;
+    //constructor
+    public Computadora()
+    {
+   procesador = new Procesador();
+   procesador.Marca="AMD";
+    }
+    public void Encender()
+    {
+        Console.WriteLine("La computadora esta encendida");
+        procesador.MostrarMarca();
+    }
+}
+public class Procesador
+{
+    public string Marca {get;set;}
+    public void MostrarMarca()=>Console.WriteLine($"Procesador Marca {Marca}");
+}
+public class Program
+{
+    static void Main()
+    {
+       Computadora pc = new Computadora();
+       pc.Encender();
+    }
+}
